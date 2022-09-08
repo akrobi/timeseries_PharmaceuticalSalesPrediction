3f3a46208743bda041769848167f654e3e9b3473
akrobi <gwangui00000@gmail.com>
2022-09-08 03:12:14 +0300

    updated read and write utility files

--- scripts/dvcutils.py
+++ scripts/dvcutils.py
@@ -8,18 +8,14 @@ sys.path.append(os.path.abspath(os.path.join("./scripts/")))
 
 class ReadWriteDVC():
     def dvc_get_data(path, version='v1') :
-        try:
-            repo = "~/timeseries_PharmaceuticalSalesPrediction"
-            data_url = dvc.api.get_url(path=path, repo=repo, rev=version)
-            data_url = str(data_url)[6:]
-            df = pd.read_csv(data_url, sep=",", low_memory=False)
-        except Exception as e:
-            df = None
+        repo = "~/timeseries_PharmaceuticalSalesPrediction"
+        data_url = dvc.api.get_url(path=path, repo=repo, rev=version)
+        data_url = str(data_url)[6:]
+        df = pd.read_csv(data_url, sep=",", low_memory=False)
+        
         return df
     
     def to_csv(df, csv_path, index=False):
-        try:
-            df.to_csv(csv_path, index=index)
-        except Exception as e:
-            # raise Error("did not write to csv file")
-            pass
+        df.to_csv(csv_path, index=index)
+        print("data written to csv file")
+        
